777d5659e95d03827e9133c6221d31da3241cb11
saarc <saarc99@naver.com>
2020-08-26 11:18:29 +0900

    Update start.sh

--- network/start.sh
+++ network/start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-#
-# Copyright IBM Corp All Rights Reserved
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-# Exit on first error, print all commands.
+
 set -ev
 
 function replacePrivateKey() {
